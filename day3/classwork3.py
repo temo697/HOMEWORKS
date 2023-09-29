fdd65226ab86b7157b831fd5b75bb97ed8ad0fa0
temo697 <145621806+temo697@users.noreply.github.com>
2023-09-30 00:37:19 +0400

    Add files via upload

---
+++ day3/classwork3.py
@@ -0,0 +1,74 @@
+# name = "tom"
+# salary = 75000
+# print(name)
+
+
+#here we learned about debugging
+
+# message = "debugging"
+# print(message)
+
+# user_name = input("what's your name")
+# print("hello " + user_name)
+
+# x = 9.34
+# y = int(x)
+# print(y)
+
+
+# a = 5
+# b = 9
+# x = str(a)
+# y = str(b)
+# print(x+y)
+
+
+# width = input()
+# height = input()
+# area = int(width) * int(height)
+# print(area-9)
+
+# x = 4
+# x *= 3
+# print(x)
+
+# x = "a"
+# x *= 3
+# print(x)
+
+
+
+# spam = "7"
+# spam = spam + "0"
+# eggs = int(spam) + 3
+# print(float(eggs))
+
+
+# print("8"+"2")
+
+#მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი და დაპრინტეთ გრძელ წინადადებაში ყველა ეს მონაცემი.
+
+# name = input()
+# surname = input()
+# age = input()
+
+# name = input("my name is")
+# surname = input("my surname is")
+# age = input("my age is")
+# age = input("how old are you")
+# new_age = int("new_age") + "25"
+# print(new_age)
+
+
+
+name = input()
+surname = input()
+age = input()
+
+name = input("user_name name is ")
+surname = input("user_surname surname is ")
+age = input("user_age age is ")
+ 
+temo = input("how old are you ")
+new_age = (int(age) + 25 )
+print(new_age)
---
+++ day3/homework3.py
@@ -0,0 +1,12 @@
+
+name = input()
+surname = input()
+age = input()
+
+name = input("user_name name is ")
+surname = input("user_surname surname is ")
+age = input("user_age age is ")
+ 
+temo = input("how old are you ")
+new_age = ("age " "+ " "25" )
+print(new_age)
